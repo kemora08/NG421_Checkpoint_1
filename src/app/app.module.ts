5de42ad3d7b2ad4de0700a669ef9cce60357a3f0
kemora08 <kevin_mora89@hotmail.com>
2020-08-30 22:48:23 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ConfirmationModalComponent } from './confirmation-modal/confirmation-mo
 import {RouterModule } from '@angular/router';
 import { BoardComponent } from './board/board.component';
 import { TodoEditComponent } from './todo-edit/todo-edit.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialTableComponent } from './material-table/material-table.component';
+import { TodoTableComponent } from './todo-table/todo-table.component';
 
 const routes = [];
 const appRoutes = RouterModule.forRoot(routes);
@@ -22,13 +25,16 @@ const appRoutes = RouterModule.forRoot(routes);
     CreateTodoComponent,
     ConfirmationModalComponent,
     BoardComponent,
-    TodoEditComponent
+    TodoEditComponent,
+    MaterialTableComponent,
+    TodoTableComponent
   ],
   imports: [
     BrowserModule,
     appRoutes,
     FormsModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
